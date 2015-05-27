cc3858cd15ba8d80a9b1ee56c2a24355171fcddf
llch <859988748@qq.com>
2015-05-27 17:48:29 +0800

    Initial Commit

---
+++ VirginPHPCommunicateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VirginPHPCommunicateTest
+//
+//  Created by llch on 15/5/27.
+//  Copyright (c) 2015年 llch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VirginPHPCommunicateTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VirginPHPCommunicateTest
+//
+//  Created by llch on 15/5/27.
+//  Copyright (c) 2015年 llch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
